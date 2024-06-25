dd35ddd25add21a2ece1c37b8b35076ed19c6b0f
c_edward@live.ca <c_edward@live.ca>
2024-06-25 18:14:28 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +9,22 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def clear_input(self):
+    self.Name_box.text = ""
+    self.Email_box.text = ""
+    self.Feed_backBox.text = ""
+  
+  def Submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.Name_box.text
+    email = self.Email_box.text
+    feedback = self.Feed_backBox.text
+
+    anvil.server.call('send_feedback', name, email, feedback)
+    Notification("Thanks for submitting").show()
+
+    self.clear_inputs()
+
+
+
