ef001e7b3c6c8c089996823208cb15f39d23fbd0
c_edward@live.ca <c_edward@live.ca>
2024-07-29 17:27:27 +0000

    Edited Gantt_Test

--- client_code/Gantt_Test/__init__.py
+++ client_code/Gantt_Test/__init__.py
@@ -11,14 +11,13 @@ class Gantt_Test(Gantt_TestTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    
-    
   # Any code you write here will run before the form opens.
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
     img = anvil.server.call('run_plotly1_uplink')
-    width, height = anvil.image.get_dimensions(img)
+    width = img.layout.width
+    height = img.layout.height
     alert(f"The image is {width} pixels wide and {height} pixels high")
     self.plot_1.figure = img
   
